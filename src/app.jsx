a01f095452f13e118ef9b0f55fdcfb7d11fbaaa4
amci1203 <allen.mcintoshii@gmail.com>
2016-12-11 15:38:05 -0500

    Made a few generic changes

--- src/app.jsx
+++ src/app.jsx
@@ -4,17 +4,25 @@ import { render } from 'react-dom';
 import MainNav from './components/main-nav.jsx';
 import MainHeader from './components/main-header.jsx';
 import Why from './components/why-section.jsx';
+import About from './components/about.jsx';
 
-function main (props) {
-  return(
-    <div>
-      <MainNav candidateName='Allen McIntosh II' />
-      <div id='landing'>
-        <MainHeader />
-        <Why />
+export default class main extends React.Component {
+  constructor () {
+    super();
+  }
+
+  render (props) {
+    return(
+      <div>
+        <MainNav candidateName='Allen McIntosh II' />
+        <div id='landing' className='fullscreen'>
+          <MainHeader />
+          <Why />
+        </div>
+        <About />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 render(React.createElement(main), document.getElementById('app'))
--- src/components/main-header.jsx
+++ src/components/main-header.jsx
@@ -4,7 +4,7 @@ export default function MainHeader (props) {
   return (
     <header className='header text-center'>
       <div className='wrapper'>
-        <h1>Good Day</h1>
+        <h1 className='section-title'>Good Day</h1>
         <h3>...to the HR department (and anybody else who may be see this) at &yet <br /> (a weird at first, but admittedly unique name)</h3>
       </div>
     </header>
