2986aa57611a43abe7a479e4d53c1c1cd3ee3cd3
amci1203 <allen.mcintoshii@gmail.com>
2016-12-11 21:05:15 -0500

    Completed tab functionality; basic styling done

---
+++ src/components/qualifications.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Qualifications (props) {
+  return (
+    <ul>
+      <li>THIS IS A TEST</li>
+    </ul>
+  )
+}
---
+++ src/components/skills.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Skills (props) {
+  return (
+    <ul>
+      <li>THIS IS A TEST ALSO</li>
+    </ul>
+  )
+}
